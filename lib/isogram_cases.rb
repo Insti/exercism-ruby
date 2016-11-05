a14029093769ee369f412867a33515a4906cce09
Geoff Hubbard <ghub@instihost.com>
2016-11-05 18:58:25 +0000

    Generate whole test body in generator.

--- lib/isogram_cases.rb
+++ lib/isogram_cases.rb
@@ -1,12 +1,45 @@
-class IsogramCase < OpenStruct
+require 'ostruct'
 
-  def name
-    format('test_%s', description)
+class TestCase
+  attr_reader :data
+  def initialize(json_data)
+    @data = OpenStruct.new(json_data)
   end
 
-  def description
-    input.downcase.gsub(/[ -]/,'_')
+  def method_definition
+    format 'def %s', test_name
   end
+
+  def test_name
+    format 'test_%s', @data['description'].downcase.tr_s(' -','_')
+  end
+
+  def workload
+    body = skip,
+      format( "string = '%s'", data.input ),
+      format( "%s Isogram.is_isogram?(string)", assertion)
+    [indent( body ),nil].join("\n")
+  end
+
+  def indent(array, count=4 )
+    array.map { |line| format "%s%s", ' ' * count, line }
+  end
+end
+
+class IsogramCase < TestCase
+
+  def input
+    data.input
+  end
+
+  def index
+    data.index
+  end
+
+  def expected
+    data.expected
+  end
+
 
   def assertion
     expected ? 'assert' : 'refute'
@@ -18,7 +51,8 @@ class IsogramCase < OpenStruct
 end
 
 IsogramCases = proc do |data|
-  JSON.parse(data)['cases'].map.with_index do |row, i|
-    IsogramCase.new(row.merge('index' => i))
+  JSON.parse(data)['cases'].map.with_index do |test_case,index| 
+    test_case['index'] = index
+    IsogramCase.new(test_case)
   end
 end
