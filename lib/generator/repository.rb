463ccc8493d46543fb0904a38da872489b767b4e
Geoff Hubbard <ghub@instihost.com>
2017-05-19 12:27:20 +0100

    Get the slugs of all the problems in a track.

--- lib/generator/repository.rb
+++ lib/generator/repository.rb
@@ -1,4 +1,17 @@
 module Generator
+  class Repository
+    attr_reader :paths
+    def initialize(paths:)
+      @paths = paths
+    end
+
+    def track_problems
+      Dir.glob(File.join(paths.track, 'exercises', '*')).map do |path|
+        Pathname.new(path).basename.to_s
+      end
+    end
+  end
+
   class ExerciseRepository
     include Files::TrackFiles
     include Files::MetadataFiles
--- test/generator/repository_test.rb
+++ test/generator/repository_test.rb
@@ -1,6 +1,19 @@
 require_relative '../test_helper'
 
 module Generator
+  class RepositoryTest < Minitest::Test
+    FixturePaths = Paths.new(
+      metadata: 'test/fixtures/metadata',
+      track: 'test/fixtures/xruby'
+    )
+
+    def test_track_problems
+      subject = Repository.new(paths: FixturePaths)
+      expected = ['alpha', 'alpha-beta', 'beta']
+      assert_equal expected, subject.track_problems
+    end
+  end
+
   class ExerciseRepositoryTest < Minitest::Test
     def test_construction
       subject = ExerciseRepository.new(paths: nil, slug: nil)
